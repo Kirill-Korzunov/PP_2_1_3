2423ff97e37df243673a3ef5c60b4f520eefb817
Kirill-Korzunov <kirillkorzunov9000@gmail.com>
2025-01-29 00:39:22 +0300

    first commit

---
+++ src/main/java/app/model/Timer.java
@@ -0,0 +1,17 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Timer {
+
+    private final Long nanoTime;
+
+    public Timer() {
+        this.nanoTime = System.nanoTime();
+    }
+
+    public Long getTime() {
+        return nanoTime;
+    }
+}
